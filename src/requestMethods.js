b9f8f08e6c20493012fc71293da905df348a1122
Amine Boukhari <aminetech21@outlook.com>
2022-03-14 23:58:01 +0100

    access Token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://amine-mern-stack-e-commerce.herokuapp.com/api/"
-// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMjFmYTlhZGEzMTZiZTRhZDU0NTc3NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzEzNzQ2MywiZXhwIjoxNjQ3Mzk2NjYzfQ.7R3EvH8COwlgK3wqc4r4Q09YGc-hgWPX9WBan627Xo0"
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
