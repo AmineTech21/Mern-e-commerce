8e61553ee4842e97b52803448687129fe2d1daee
Amine Boukhari <aminetech21@outlook.com>
2022-03-14 16:13:29 +0100

    new added api links

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://amine-mern-stack-e-commerce.herokuapp.com/api/"
+const BASE_URL = "https://amine-mern-stack-e-commerce.herokuapp.com/"
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMjFmYTlhZGEzMTZiZTRhZDU0NTc3NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzEzNzQ2MywiZXhwIjoxNjQ3Mzk2NjYzfQ.7R3EvH8COwlgK3wqc4r4Q09YGc-hgWPX9WBan627Xo0"
 
