10e8ac653a191dd302e6576215fea6791481146f
Amine Boukhari <aminetech21@outlook.com>
2022-03-15 02:13:24 +0100

    access Token

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -8,6 +8,7 @@ export const login = async (dispatch, user) => {
   try {
     const res = await publicRequest.post("/auth/login", user);
     dispatch(loginSuccess(res.data));
+    dispatch(emptyCart())
   } catch (err) {
     dispatch(loginFailure());
   }
